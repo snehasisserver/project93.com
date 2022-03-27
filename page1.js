b9ab99008ea827507ad16600d3f9cf06651d3f11
Snehasis Ghosh <91667746+snehasisserver@users.noreply.github.com>
2022-03-27 15:47:44 +0530

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,16 @@
+function in_sub(){
+   var name = document.getElementById("fname").value;
+   var email =  document.getElementById("ename").value;
+   var numberph = document.getElementById("pno").value;
+   var passwordvar =  document.getElementById("password").value;
+
+   localStorage.setItem("username",name);
+   localStorage.setItem("email",email);
+   localStorage.setItem("Phone_number",numberph);
+   localStorage.setItem("password",passwordvar);
+
+   document.getElementById("fname").innerHTML = "";
+   document.getElementById("ename").innerHTML = "";
+   document.getElementById("pno").innerHTML = "";
+   document.getElementById("password").innerHTML = "";
+}
